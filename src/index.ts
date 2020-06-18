285ca68b0ac01487501340e680335829a358fc98
RayhanHamada <hamadadesk1999@gmail.com>
2020-06-18 13:10:49 +0700

    working on write option

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,35 @@
+import fs from 'fs/promises';
+
+function errorLog(msg: string) {
+  console.log(`dotenv-file-maker: ${msg}`);
+}
+
+type As = 'normal' | 'inJson';
+
 /**
- * TODO: make dotenv-file-maker func, like function(path, 'normal' | 'inJson')
+ * make .env file in specified path
+ * @param path path for the .env to be generated
+ * @param envListPath path of envList file
+ * @param as { As } what format the generated file should be
  */
+export default async function (
+  path: string,
+  envListPath = './envlist',
+  as: As = 'normal'
+): Promise<void> {
+  /**
+   * read envListPath
+   */
+  const rawEnv = await fs
+    .readFile(envListPath, 'utf-8')
+    .catch(() => errorLog('failed reading file'));
+
+  /**
+   * parse rawEnv to list
+   */
+  const envList = (rawEnv as string).split('\n');
+
+  /**
+   * TODO: make write option depends on the format
+   */
+}
